735de6626809152507477006461a3c92d1936cd9
thfhthdthdbs <thfhthdthdbs@snu.ac.kr>
2024-10-31 10:39:10 +0000

    첫 웹사이트 개꿀

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("🎈 우히히")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+st.title("👋🏻 Streamlit 앱 만들기")
+st.subheader("예비교원 워크숍 10월 특강 실습 페이지")
